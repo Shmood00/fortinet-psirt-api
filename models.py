ef0b665e0d7261d8cd5c064b52127b3571cb57d6
shmood00 <43577426+Shmood00@users.noreply.github.com>
2023-09-20 17:31:42 -0400

    Recreated models

--- models.py
+++ models.py
@@ -1,25 +1,22 @@
 from pydantic import BaseModel
 from typing import List, Dict
 
+class CVVS3(BaseModel):
+    base_score: float
+    vector: str
 class Affected(BaseModel):
     ge: str
     le: str
 
-
-class Nodes(BaseModel):
+class SubNodes(BaseModel):
     description: str
     cpe: str
     affected: Affected
     fixed_in: List[str]
 
-
-class CVSSV3(BaseModel):
-    base_score: float
-    vector: str
-
 class Configurations(BaseModel):
-    nodes: List[Nodes]
-    cvssv3: CVSSV3
+    nodes: List[SubNodes]
+    cvssv3: CVVS3
     cwe: List[str]
     impact: str
 
@@ -27,19 +24,20 @@ class Vulnerabilities(BaseModel):
     id: str
     cve: str
     definitions: List[Configurations]
+    
 
-
-class AllPSIRT(BaseModel):
+class PSIRT(BaseModel):
     id: str
     title: str
     summary: str
-    vulnerabilites: List[Vulnerabilities]
+    vulnerabilities: List[Vulnerabilities]
     published: str
     updated: str
     advisory_url: str
 
+
 """ Model for vulnerabilities affected by given OS and version """
 class VersionAffected(BaseModel):
-    version: List[str]
+    vulnerabilities: List[str]
     fixed_in: List[str]
     is_vulnerable: bool
