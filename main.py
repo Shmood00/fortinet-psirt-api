cff4944a4f8be6a53ebc7bb63f5596123ebd9f7c
shmood00 <43577426+Shmood00@users.noreply.github.com>
2023-09-19 20:57:06 -0400

    Applied models to API endpoints

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import models
 from packaging import version
+from typing import List, Union
 import json, uvicorn
 
 app = FastAPI(
@@ -31,17 +33,27 @@ def compare_versions(v1,v2):
     elif version.parse(v1) >= version.parse(v2):
         return 1
 
-@app.get("/")
+@app.get("/", name="All PSIRTs", summary="Returns all Fortinet PSIRTs published in 2023.",
+        tags=["All Vulnerabilities"],
+        response_description="Returns list of dictionaries containing PSIRT advisories posted so far in 2023.",
+        response_model=List[models.AllPSIRT]
+)
 async def root():
     return data
 
-@app.get("/psirt/{psirt_id}")
+@app.get("/psirt/{psirt_id}", name="Individual PSIRT", summary="Returns information on requested PSIRT",
+         tags=["Individual PSIRT"],
+         response_description="Returns a dictionary containing information on requested PSIRT",
+         response_model=models.AllPSIRT)
 async def individual_psirt(psirt_id):
     for vuln in data:
         if vuln['id'] == psirt_id:
             return vuln
 
-@app.get("/psirt/vulnerabilities/{os_type}")
+@app.get("/psirt/vulnerabilities/{os_type}", name="OS Version Affected", summary="Returns vulnerabilities the given OS and version are affected by.",
+         tags=["Vulnerabilities"],
+         response_description="Returns a dictionary of vulnerabilities that impact the given OS and version type.",
+         response_model=models.VersionAffected)
 async def find_vuln(os_type: str, os_version: str):
     matched_vulns = {}
     vuln_names = []
