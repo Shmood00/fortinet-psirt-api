a96779043524b05aa3ecd1a01ac92ce4a2a8c8cd
shmood00 <43577426+Shmood00@users.noreply.github.com>
2023-09-24 17:58:28 -0400

    adjusted cvss scores model

--- models.py
+++ models.py
@@ -45,10 +45,11 @@ class PSIRT(BaseModel):
 
 """ Model for vulnerabilities affected by given OS and version """
 class CVSSV3(BaseModel):
-    low: Optional[List[float]] = None
-    medium: Optional[List[float]] = None
-    high: Optional[List[float]] = None
-    critical: Optional[List[float]] = None
+    """ Used to be just List[float] not List[List[float]] """
+    low: Optional[List[List[float]]] = None
+    medium: Optional[List[List[float]]] = None
+    high: Optional[List[List[float]]] = None
+    critical: Optional[List[List[float]]] = None
 
 class VersionAffected(BaseModel):
     vulnerabilities: Optional[List[str]] = None
